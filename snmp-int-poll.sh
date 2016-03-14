ec55f8c65a49fe8cd48fc1ae2e4a822c88902988
Kevin <masonke@gmail.com>
2016-03-14 10:32:12 -0400

    Added firstrun check to avoid large spikes
    Added the firstrun check to avoid large spikes caused when there i nothing in $AvgInbpsOld & $AvgOutbpsOld
    
    Cleaned up the output selection

--- snmp-int-poll.sh
+++ snmp-int-poll.sh
@@ -4,16 +4,15 @@
 # The value pulled is in Bytes and is a cumulative counter
 # Next, convert to bits (bps) and divide by the POLLRATE to create an Average bps
 #
-# Other OIDs can be polled, this is the most common use case.
-#
 #1.3.6.1.2.1.31.1.1.1.1 = IF-MIB::ifName
 #1.3.6.1.2.1.31.1.1.1.6 = IF-MIB::ifHCOutOctets Bytes
 #1.3.6.1.2.1.31.1.1.1.10 = IF-MIB::ifHCInOctets Bytes
 #1.3.6.1.2.1.31.1.1.1.15 = IF-MIB::ifHighSpeed  Int Speed in Megabits/s
-#  http://tools.cisco.com/Support/SNMP/do/BrowseOID.do?local=en&translate=Translate&objectInput=1.3.6.1.2.1.31.1.1.1
+# http://tools.cisco.com/Support/SNMP/do/BrowseOID.do?local=en&translate=Translate&objectInput=1.3.6.1.2.1.31.1.1.1
 
 POLLRATE=10
-DEVICE=10.30.254.200
+OutputSelect=1
+DEVICE=10.89.254.200
 #CSTRING=y9ugiq7vEqOB
 CSTRING=wPeZ6kiG42oZhg
 # Set the IfIndex for the device here using "sh snmp mib ifmib ifindex eth 0/0"
@@ -30,12 +29,15 @@ AvgInbps=0
 AvgOutbps=0
 AvgInbpsOld=0
 AvgOutbpsOld=0
+FirstRun=1
+
 
 #Grab interface name, this does not change, so grab it once
 IfName=$(snmpget -v2c -c $CSTRING $DEVICE 1.3.6.1.2.1.31.1.1.1.1.$IfIndex | awk '{print $4}' )
 
 #echo Date,Time,InOctets,OutOctets,AvgInBPS,AvgOutBPS
 
+## main polling loop
 while [ "a" != "b" ]
 do
         DAY=$(date +%F)
@@ -43,21 +45,35 @@ do
         IfInOctets=$(snmpget -v2c -c $CSTRING $DEVICE 1.3.6.1.2.1.31.1.1.1.6.$IfIndex | awk '{print $4}')
         IfOutOctets=$(snmpget -v2c -c $CSTRING $DEVICE 1.3.6.1.2.1.31.1.1.1.10.$IfIndex  | awk '{print $4}')
         IfHighSpeed=$(snmpget -v2c -c $CSTRING $DEVICE 1.3.6.1.2.1.31.1.1.1.15.$IfIndex  | awk '{print $4}')
+
+# Convert Octets to bits       
         AvgInbpsNew=$(($IfInOctets*8/$POLLRATE))
         AvgOutbpsNew=$(($IfOutOctets*8/$POLLRATE))
 
+# Find the difference from the previous run through 
         let AvgInbps=$AvgInbpsNew-$AvgInbpsOld
         let AvgOutbps=$AvgOutbpsNew-$AvgOutbpsOld
 
+# Make this run's values the previous run
         AvgInbpsOld=$AvgInbpsNew
         AvgOutbpsOld=$AvgOutbpsNew
 
+# Check if this is the first run through the polling. If it is, do not print out the values to prevent a counter spike
+    if [ $FirstRun == 0 ]
+      then
+        if [ $OutputSelect == 1 ] 
+          then
 # Key Value Pair for Sumo
-        echo DAY:$DAY TIME:$TIME IfName:$IfName IfSpeed:$IfHighSpeed INOctets:$IfInOctets INOutOctets:$IfOutOctets AvgInbps:$AvgInbps AvgOutbps:$AvgOutbps
-
+            echo DAY:$DAY TIME:$TIME IfName:$IfName IfSpeed:$IfHighSpeed INOctets:$IfInOctets INOutOctets:$IfOutOctets AvgInbps:$AvgInbps AvgOutbps:$AvgOutbps
+          else
 # CSV output
-#        echo $DAY,$TIME,$IfInOctets,$IfOutOctets,$AvgInbps,$AvgOutbps
-
+           echo $DAY,$TIME,$IfName,$IfHighSpeed,$IfInOctets,$IfOutOctets,$AvgInbps,$AvgOutbps
+        fi
+      fi
+      
+# Now that a run has been completed, set FirtsRun to 0 so values are printed
+    FirstRun=0
         sleep $POLLRATE
 
 done
+
